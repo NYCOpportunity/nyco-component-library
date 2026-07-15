36276fe0d1c355421f63de19a72735ca462f1044
yuriyberezskyy <bereza95@gmail.com>
2026-07-15 14:16:02 -0400

    fix: remove unused safelist patterns from Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{ts,tsx}', './.storybook/**/*.{ts,tsx}'],
-  safelist: [
-    // Width utilities
-    { pattern: /^w-/ },
-    { pattern: /^min-w-/ },
-    { pattern: /^max-w-/ },
-    // Padding utilities
-    { pattern: /^p-/ },
-    { pattern: /^px-/ },
-    { pattern: /^py-/ },
-    // Margin utilities
-    { pattern: /^m-/ },
-    { pattern: /^mx-/ },
-    { pattern: /^my-/ },
-    // Shadow utilities
-    { pattern: /^shadow/ },
-    // Rounding utilities
-    { pattern: /^rounded/ },
-    // Border utilities
-    { pattern: /^border/ },
-    // Opacity utilities
-    { pattern: /^opacity/ },
-    // Transform utilities
-    { pattern: /^scale/ },
-    { pattern: /^rotate/ },
-    // Important modifier
-    { pattern: /^!/ },
-  ],
   theme: {
     extend: {
       fontFamily: {
